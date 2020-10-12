f844db9351abdf77ddf8cc520490d4eecfc0f818
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-10-12 10:48:44 -0700

    Starts drawing the background.

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { GridManagerService } from 'src/app/services/grid-manager.service';
 
 @Component({
@@ -11,6 +11,15 @@ export class BackgroundComponent implements OnInit {
   @Input() devMode: boolean;
   @Input() rows: number;
 
+  @ViewChild('b1', { static: true })
+  private _bgMap: ElementRef<HTMLImageElement>;
+
+  // @ViewChild('c1', { static: true })
+  // private _cobble: ElementRef<HTMLImageElement>;
+
+  @ViewChild('bgCanvas', { static: true })
+  private _canvas: ElementRef<HTMLCanvasElement>;
+
   public canvasSize: [number, number] = [64, 64];
   public colset: number[];
   public rowset: number[];
@@ -22,6 +31,46 @@ export class BackgroundComponent implements OnInit {
     this.canvasSize[1] = this.columns * 64;
     this.colset = new Array(this.columns);
     this.rowset = new Array(this.rows);
+
+    this._bgMap.nativeElement.onload = this._buildBackground.bind(this);
+  }
+
+  private _buildBackground(): void {
+    const ctx = this._canvas.nativeElement.getContext('2d');
+    for (let col = 0; col < this.columns; col++) {
+      ctx.drawImage(
+        this._bgMap.nativeElement,
+        64,
+        1792,
+        64,
+        64,
+        (col * 64),
+        128,
+        64,
+        64);
+      
+      ctx.drawImage(
+        this._bgMap.nativeElement,
+        64,
+        1792,
+        64,
+        64,
+        (col * 64),
+        320,
+        64,
+        64);
+      
+      // ctx.drawImage(
+      //   this._cobble.nativeElement,
+      //   0,
+      //   0,
+      //   64,
+      //   64,
+      //   (col * 64),
+      //   256,
+      //   64,
+      //   64);
+    }
   }
 
 }
