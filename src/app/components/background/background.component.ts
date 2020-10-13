10228625d5ca6320132010874e6230c0ed4c6473
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-10-13 10:45:43 -0700

    Adds some background graphics for walking areas.

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { GridManagerService } from 'src/app/services/grid-manager.service';
 
+let imageLoadCount = 0;
+
 @Component({
   selector: 'tt-background',
   templateUrl: './background.component.html',
@@ -14,8 +16,8 @@ export class BackgroundComponent implements OnInit {
   @ViewChild('b1', { static: true })
   private _bgMap: ElementRef<HTMLImageElement>;
 
-  // @ViewChild('c1', { static: true })
-  // private _cobble: ElementRef<HTMLImageElement>;
+  @ViewChild('c1', { static: true })
+  private _cobble: ElementRef<HTMLImageElement>;
 
   @ViewChild('bgCanvas', { static: true })
   private _canvas: ElementRef<HTMLCanvasElement>;
@@ -33,9 +35,14 @@ export class BackgroundComponent implements OnInit {
     this.rowset = new Array(this.rows);
 
     this._bgMap.nativeElement.onload = this._buildBackground.bind(this);
+    this._cobble.nativeElement.onload = this._buildBackground.bind(this);
   }
 
   private _buildBackground(): void {
+    imageLoadCount++;
+    if (imageLoadCount < 2) {
+      return;
+    }
     const ctx = this._canvas.nativeElement.getContext('2d');
     for (let col = 0; col < this.columns; col++) {
       ctx.drawImage(
@@ -59,17 +66,50 @@ export class BackgroundComponent implements OnInit {
         320,
         64,
         64);
+
+      ctx.drawImage(
+        this._bgMap.nativeElement,
+        64,
+        1280,
+        64,
+        64,
+        (col * 64),
+        64,
+        64,
+        64);
       
-      // ctx.drawImage(
-      //   this._cobble.nativeElement,
-      //   0,
-      //   0,
-      //   64,
-      //   64,
-      //   (col * 64),
-      //   256,
-      //   64,
-      //   64);
+      ctx.drawImage(
+        this._bgMap.nativeElement,
+        64,
+        1408,
+        64,
+        64,
+        (col * 64),
+        384,
+        64,
+        64);
+      
+      ctx.drawImage(
+        this._cobble.nativeElement,
+        0,
+        0,
+        256,
+        256,
+        (col * 64),
+        192,
+        64,
+        64);
+      
+      ctx.drawImage(
+        this._cobble.nativeElement,
+        0,
+        0,
+        256,
+        256,
+        (col * 64),
+        256,
+        64,
+        64);
     }
   }
 
